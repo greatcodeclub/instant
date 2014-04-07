62580c2005b29579665c8adbb8df2533e3043ea8
macournoyer <macournoyer@gmail.com>
2014-04-07 14:54:18 -0400

    Better demo node server.

--- demo/simple_node_server.js
+++ demo/simple_node_server.js
@@ -1,7 +1,14 @@
 var http = require('http')
 
 var server = http.createServer(function (req, res) {
-  res.end(req.method + ' ' + req.url)
+  var body = req.method + ' ' + req.url
+
+  res.writeHead(200, {
+    'Content-Length': body.length,
+    'Content-Type': 'text/html'
+  })
+
+  res.end(body)
 })
 
 server.listen(3000)
