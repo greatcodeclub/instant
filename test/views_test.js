0693c7d78e98da637ee8846e8526ca166f9d9636
macournoyer <macournoyer@gmail.com>
2014-04-08 11:20:29 -0400

    Implement view rendering.

--- test/views_test.js
+++ test/views_test.js
@@ -2,7 +2,7 @@ var assert = require('assert'),
     App = require('../lib/app').App
     Response = require('../lib/response').Response
 
-xdescribe('Views', function() {
+describe('Views', function() {
   beforeEach(function() {
     this.app = new App()
     this.app.set('views', __dirname + '/views')
@@ -13,13 +13,13 @@ xdescribe('Views', function() {
     assert.equal(this.app.settings['view engine'], 'jade')
   })
 
-  xit('render from app', function() {
+  it('render from app', function() {
     this.app.render('index', { content: 'hi' }, function(html) {
       assert.equal(html, '<p>hi</p>')
     })
   })
 
-  xit('render from response', function() {
+  it('render from response', function() {
     var html, res = new Response()
     res.app = this.app
 
