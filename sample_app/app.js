828ba37046293c4cda1af7116882b3551ed13ccb
macournoyer <macournoyer@gmail.com>
2014-04-08 11:22:04 -0400

    Add middlewares in sample app.

--- sample_app/app.js
+++ sample_app/app.js
@@ -1,9 +1,14 @@
-var instant = require('../lib/instant')
+var instant = require('../lib/instant'),
+    logger = require('morgan'),
+    serveStatic = require('serve-static')
 
 var app = instant()
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
+
+app.use(logger())
+app.use(serveStatic(__dirname + '/public'))
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Instant' })
