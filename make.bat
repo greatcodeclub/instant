e416b9e1c7662a9a3a4147a22762327ebf73d407
macournoyer <macournoyer@gmail.com>
2014-05-15 21:06:22 -0400

    Add batch file to mimic make on Windows.

---
+++ make.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM This batch file mimics what the Makefile does on Windows
+
+FOR /F "tokens=*" %%i IN ('npm bin') DO SET BIN=%%i
+
+if "%1" == "test" (
+  %BIN%\mocha --reporter spec
+  goto DONE
+)
+
+if "%1" == "watch" (
+  %BIN%\mocha --watch --reporter min
+  goto DONE
+)
+
+if "%1" == "watch-app" (
+  %BIN%\nodemon sample_app\app.js
+  goto DONE
+)
+
+:DONE
